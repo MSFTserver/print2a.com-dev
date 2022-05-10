ab175175586f0fb55b2d5d8d0625e84b8bd052bf
MSFTserver <MSFTserver@gmail.com>
2022-05-09 22:33:11 -0700

    fix latest page spamming api

--- static/latest.js
+++ static/latest.js
@@ -4,19 +4,23 @@ import { Post } from '../src/components/Post';
 const print2aApiHost = "https://print2a.com";
 const print2aApiPort = "5757";
 const print2aApiEndpoint = `${print2aApiHost}:${print2aApiPort}`;
+let count = 0;
 
 const GetLatest = () => {
   const [latest, setLatest] = useState([{title: "LOADING...", tags: "LOADING...", link: "#"}]);
   useEffect(() => {
-    if (typeof window !== "undefined"){
-      async function getLatest(){
+    if (typeof window !== 'undefined') {
+      async function getLatest() {
         const req = await fetch(`${print2aApiEndpoint}/LatestProjects`);
         const res = await req.json();
         setLatest(res);
       }
-      getLatest();
+      if (count <= 0) {
+        getLatest();
+        count = 1;
+      }
     }
-  }, [latest])
+  }, [latest]);
   return (
     latest.map((file, index) => (
     <Post
