04906a46cdf0c69c570ced2e96a693ede2136649
MSFTserver <MSFTserver@gmail.com>
2022-06-30 23:11:42 -0700

    update mobile theme

--- src/components/shared/NavBar/NavBar.js
+++ src/components/shared/NavBar/NavBar.js
@@ -1,10 +1,30 @@
 /* eslint-disable react/prefer-stateless-function */
 import './NavBar.scss'
 import React from 'react'
-import { Button, Header, Heading, Row } from 'arwes'
+import { Button, Header, Heading, Row, Words } from 'arwes'
 import { Link } from 'react-router-dom'
 
+function mobileMenuClicked() {
+  const isActive = document.getElementById('navLinks').style.display
+  console.log("test",isActive)
+  if (isActive === 'none') {
+    isActive.style.display = 'block'
+  } else if (isActive === 'block') {
+    isActive.style.display = 'none'
+  } else {
+    isActive.style.display = 'block'
+  }
+}
+
 class NavBar extends React.Component {
+
+  componentDidMount() {
+    console.log("fired")
+    console.log(document.getElementById('mobileMenu').onclick)
+    document.getElementById('mobileMenu').addEventListener('click', () => mobileMenuClicked())
+    console.log(document.getElementById('mobileMenu').onclick)
+  }
+
   render() {
     const location = window.location.pathname
     if (location === '/') {
@@ -20,54 +40,62 @@ class NavBar extends React.Component {
     } else {
       this.props.state.showPage = 'home'
     }
+
     return (
       <div className="NavBar">
         <Header animate show={this.props.anim.entered}>
           <nav className="container-fluid navbar">
-            <Heading>Print2a</Heading>
+            <Heading>
+              Print2a
+              <li id="mobileMenu" class="fa fa-bars" title="Click me!"/>
+            </Heading>
             <Row className="row wrap mr-1" id="navLinks" col s={12}>
-              <Link to="/" id="navToHomePage">
+              <Link to="/">
                 <Button
+                  className="navToButton"
                   animate
                   disabled={this.props.state.showPage === 'home'}
                   show={this.props.anim.entered}
                   onClick={this.props.setShowPage}
                   onMouseEnter={() => this.props.sounds.hover.play()}
                 >
-                  Home
+                  <Words className="navToText">Home</Words>
                 </Button>
               </Link>
-              <Link to="/latest" id="navToLatest">
+              <Link to="/latest">
                 <Button
+                  className="navToButton"
                   animate
                   disabled={this.props.state.showPage === 'latest'}
                   show={this.props.anim.entered}
                   onClick={this.props.setShowPage}
                   onMouseEnter={() => this.props.sounds.hover.play()}
                 >
-                  Latest
+                  <Words className="navToText">Latest</Words>
                 </Button>
               </Link>
-              <Link to="/links" id="navToLinks">
+              <Link to="/links">
                 <Button
+                  className="navToButton"
                   animate
                   disabled={this.props.state.showPage === 'links'}
                   show={this.props.anim.entered}
                   onClick={this.props.setShowPage}
                   onMouseEnter={() => this.props.sounds.hover.play()}
                 >
-                  Links
+                  <Words className="navToText">Links</Words>
                 </Button>
               </Link>
-              <Link to="/browse" id="navToBrowse">
+              <Link to="/browse">
                 <Button
+                  className="navToButton"
                   animate
                   disabled={this.props.state.showPage === 'browse'}
                   show={this.props.anim.entered}
                   onClick={this.props.setShowPage}
                   onMouseEnter={() => this.props.sounds.hover.play()}
                 >
-                  Browse
+                  <Words className="navToText">Browse</Words>
                 </Button>
               </Link>
             </Row>
