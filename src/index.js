4cd3693ff24810196ee829b79133a57c33bff394
MSFTserver <MSFTserver@gmail.com>
2022-07-14 23:28:07 -0700

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -7,12 +7,7 @@ import '@fortawesome/fontawesome-free/js/all'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'react-image-lightbox/style.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-)
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
