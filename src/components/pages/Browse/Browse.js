d300c1eda61b237f881b54764b8f5c236319cf9b
MSFTserver <MSFTserver@gmail.com>
2022-07-15 17:03:25 -0700

    update background

--- src/components/pages/Browse/Browse.js
+++ src/components/pages/Browse/Browse.js
@@ -9,11 +9,19 @@ class Browse extends React.Component {
     const { props } = this
     return (
       <div className="Browse">
-        <div className="browseBox">
-          <div className="browseContents">
-            <ChonkyBrowse {...props} />
+        <Frame
+          animate
+          level={3}
+          corners={6}
+          layer="primary"
+          show={this.props.anim.entered}
+        >
+          <div className="browseBox">
+            <div className="browseContents">
+              <ChonkyBrowse {...props} />
+            </div>
           </div>
-        </div>
+        </Frame>
       </div>
     )
   }
